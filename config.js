ee4855b0053bdb3e595083355398f5450ea73cec
joaquin.s <custodiamdevelopment.js@gmail.com>
2026-02-28 20:06:44 -0300

    modal reservas calendar

--- config.js
+++ config.js
@@ -1,1 +1,23 @@
-export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const hostname = window.location.hostname;
+
+const isLocalhost =
+  hostname === 'localhost' ||
+  hostname === '127.0.0.1';
+
+const isLocalIP =
+  hostname.startsWith('192.168.') ||
+  hostname.startsWith('10.') ||
+  hostname.startsWith('172.');
+
+let backendUrl;
+
+if (isLocalhost) {
+  backendUrl = import.meta.env.VITE_BACKEND_LOCALHOST;
+} else if (isLocalIP) {
+  backendUrl = import.meta.env.VITE_BACKEND_LOCAL_IP;
+} else {
+  // Producción (Netlify)
+  backendUrl = import.meta.env.VITE_BACKEND_URL;
+}
+
+export { backendUrl };
